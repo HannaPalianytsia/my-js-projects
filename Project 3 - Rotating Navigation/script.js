4adfa337154a32e7d8b4f8bf3c57450c4a1ddebb
HannaPalianytsia <hannapalianytsia1@gmail.com>
2025-01-21 19:52:15 +0100

    add links

--- Project 3 - Rotating Navigation/script.js
+++ Project 3 - Rotating Navigation/script.js
@@ -1,7 +1,12 @@
 const open = document.getElementById("open");
 const close = document.getElementById("close");
+const links = document.querySelectorAll("a");
 const container = document.querySelector(".container");
 
 open.addEventListener("click", () => container.classList.add("show-nav"));
 
 close.addEventListener("click", () => container.classList.remove("show-nav"));
+
+links.forEach((link) =>
+  link.addEventListener("click", () => container.classList.remove("show-nav"))
+);

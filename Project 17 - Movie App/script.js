de7f8ecfbd9746a7cdba210b0f6be54c01deb59e
HannaPalianytsia <hannapalianytsia1@gmail.com>
2025-03-09 18:25:20 +0100

    adding movies to the DOM

--- Project 17 - Movie App/script.js
+++ Project 17 - Movie App/script.js
@@ -6,6 +6,7 @@ const SEARCH_URL =
 
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+const main = document.getElementById("main");
 
 getMovies(API_URL);
 
@@ -13,7 +14,46 @@ async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
 
-  console.log(data.results);
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  console.log(movies[0]);
+
+  main.innerHTML = "";
+  movies.forEach((movie) => {
+    const { title, overview, poster_path, vote_average } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `<img
+          src="${IMG_PATH + poster_path}"
+          alt="${title}"
+        />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average.toFixed(
+      1
+    )}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>`;
+
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
 }
 
 form.addEventListener("submit", (e) => {

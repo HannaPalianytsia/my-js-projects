ec1209f4fa9d7416847a9e7ed5a6dc872fb840e7
HannaPalianytsia <hannapalianytsia1@gmail.com>
2025-03-23 17:54:54 +0100

    create drag drop event functions using this

--- Project 21 - Drag And Drop/script.js
+++ Project 21 - Drag And Drop/script.js
@@ -5,15 +5,30 @@ fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
 
 for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
 }
 
 function dragStart() {
-  console.log("drag start");
+  this.className += " hold";
+  setTimeout(() => (this.className = "invisible"), 0);
 }
 function dragEnd() {
-  console.log("drag end");
+  this.className = "fill";
 }
-function dragOver() {}
-function dragEnter() {}
-function dragLeave() {}
-function dragDrop() {}
+function dragOver(e) {
+  e.preventDefault();
+}
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += " hovered";
+}
+function dragLeave() {
+  this.className = "empty";
+}
+function dragDrop() {
+  this.className = "empty";
+  this.append(fill);
+}

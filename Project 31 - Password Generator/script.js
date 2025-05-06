5aa809cdb3c7a3278a6fa1e23cb7fb6a87d7a86f
HannaPalianytsia <hannapalianytsia1@gmail.com>
2025-05-06 17:56:02 +0200

    generate password

--- Project 31 - Password Generator/script.js
+++ Project 31 - Password Generator/script.js
@@ -1,8 +1,8 @@
 const resultEl = document.getElementById("result");
 const clipboardEl = document.getElementById("clipboard");
 const lengthEl = document.getElementById("length");
-const uppercaseEl = document.getElementById("uppercase");
 const lowercaseEl = document.getElementById("lowercase");
+const uppercaseEl = document.getElementById("uppercase");
 const numbersEl = document.getElementById("numbers");
 const symbolsEl = document.getElementById("symbols");
 const generateEl = document.getElementById("generate");
@@ -16,11 +16,42 @@ const randomFunc = {
 
 generateEl.addEventListener("click", () => {
   const length = +lengthEl.value;
-  const hasUpper = uppercaseEl.checked;
   const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
   const hasNumber = numbersEl.checked;
   const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerHTML = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
 });
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+  const typesArray = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  if (typesCount === 0) {
+    return "";
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArray.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
 
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
@@ -38,10 +69,5 @@ function getRandomSymbol() {
   const symbols = "!@#$%^&*(){}[]<>-+=,./;:";
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
-console.log(getRandomLower());
-console.log(getRandomUpper());
-console.log(getRandomNumber());
-console.log(getRandomSymbol());
 
 //https://www.w3schools.com/charsets/ref_html_ascii.asp

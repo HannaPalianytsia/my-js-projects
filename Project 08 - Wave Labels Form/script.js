f82d84fe3fceeb01a3fc12ac424d87f822767d33
HannaPalianytsia <hannapalianytsia1@gmail.com>
2025-05-11 16:11:44 +0200

    adding the wave effect

--- Project 08 - Wave Labels Form/script.js
+++ Project 08 - Wave Labels Form/script.js
@@ -1,1 +1,12 @@
-//do scripts for wave ladels
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+  const newHtml = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay: ${50 * idx}ms">${letter}</span>`
+    )
+    .join("");
+  label.innerHTML = newHtml;
+});
